b3b34ec2f5f6a6526035ee8ed0b6148299fafc9b
Juliana Pacheco <julianapacheco@MacBook-Air-de-Juliana.local>
2024-06-14 16:56:53 -0300

    atualizacao

---
+++ cypress/e2e/3-teste-lojaebac/cadastro.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
+    });
+
+    it('Completar cadastro com sucesso', () => {
+
+        
+        
+    });
+    
+});
