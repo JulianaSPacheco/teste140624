1f313ce448fd2d9d322f038e193eb3b06215a311
Juliana Pacheco <julianapacheco@MacBook-Air-de-Juliana.local>
2024-06-14 15:06:52 -0300

    átualizacao2

--- cypress/e2e/3-teste-lojaebac/login.cy.js
+++ cypress/e2e/3-teste-lojaebac/login.cy.js
@@ -11,7 +11,30 @@ describe('Funcionalidade: login', () => {
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, julianapacheco (não é julianapacheco? Sair)')
 
-
     })
+
+    it('Deve exibir msg de erro com usuário inválido', () => {
+
+        cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
+        cy.get('#username').type('pacheco@outlook.com')
+        cy.get('#password').type('Ju123456!@')
+        cy.get('.woocommerce-form > .button').click() 
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+
+        
+    });
+
+    it('Deve exibir msg de erro com senha inválida', () => {
+
+        cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
+        cy.get('#username').type('julianapacheco@outlook.com')
+        cy.get('#password').type('Ju123456')
+        cy.get('.woocommerce-form > .button').click() 
+
+        cy.get('.woocommerce-error').should('contain', 'rro: A senha fornecida para o e-mail julianapacheco@outlook.com está incorreta. Perdeu a senha?')
+
+        
+    });
 
 })
